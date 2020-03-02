8c80c549b692620b4f37ce3888750c8720648a2e
carlosmesac <carlosmesac98@gmail.com>
2020-03-02 17:57:54 +0000

    make it better

--- server/server.js
+++ server/server.js
@@ -102,6 +102,6 @@ const sendTo = (ws, message) => {
 app.use(express.static('../public'))
 
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080')
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
